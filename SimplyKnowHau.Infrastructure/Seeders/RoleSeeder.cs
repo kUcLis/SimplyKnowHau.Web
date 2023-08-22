88b450eb28902d4d1381164971dd4c44873c8b17
kUcLis <ulis89@gmail.com>
2023-08-22 21:07:29 +0200

    Added class RoleSeeder

---
+++ SimplyKnowHau.Infrastructure/Seeders/RoleSeeder.cs
@@ -0,0 +1,37 @@
+﻿using SimplyKnowHau.Domain.Entities;
+using SimplyKnowHau.Infrastructure.Persistence;
+
+
+namespace SimplyKnowHau.Infrastructure.Seeders
+{
+    public class RoleSeeder
+    {
+        private readonly SimplyKnowHauDbContext _context;
+
+        public RoleSeeder(SimplyKnowHauDbContext context) 
+        {
+            _context = context;
+        }
+
+
+        public async Task Seed()
+        {
+            if(await _context.Database.CanConnectAsync())
+            {
+                if(!_context.Roles.Any())
+                {
+                    var roles = new List<Role>
+                    {
+                       new Role{RoleName = "Admin" },
+                       new Role{RoleName = "Vet"},
+                       new Role{RoleName = "Clinic worker"},
+                       new Role{RoleName = "User"}
+                    };
+
+                    _context.Roles.AddRange(roles);
+                    await _context.SaveChangesAsync();
+                }
+            }
+        }
+    }
+}
