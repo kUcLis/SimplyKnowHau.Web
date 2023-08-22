5a7762a2caabf585f45010d6b622d19e11e5bfbe
kUcLis <ulis89@gmail.com>
2023-08-22 19:00:38 +0200

    Added new Entities, User and Role

---
+++ SimplyKnowHau.Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace SimplyKnowHau.Domain.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public string Email { get; set; } = default!;
+
+        public string PasswordHash { get; set; } = default!;
+
+        public DateTime DateOfBirth { get; set; }
+
+        public int RoleId { get; set; }
+
+        public Role Role { get; set; } = null!;
+
+        public bool EmailConfirm { get; set; } = false;
+    }
+}
