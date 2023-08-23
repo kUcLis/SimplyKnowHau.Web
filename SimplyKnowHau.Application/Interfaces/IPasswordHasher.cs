0eb869b0058355b0b3055b8de8985856473f022e
kUcLis <ulis89@gmail.com>
2023-08-23 21:30:10 +0200

    Added PasswordHasher class, added Extension for service collection to register services

---
+++ SimplyKnowHau.Application/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace SimplyKnowHau.Application.Interfaces
+{
+    public interface IPasswordHasher
+    {
+        string Hash(string email, string password);
+        bool Verify(string passwordHash, string email, string password);
+    }
+}
---
+++ SimplyKnowHau.Application/Services/PasswordHasher.cs
@@ -0,0 +1,27 @@
+﻿using SimplyKnowHau.Application.Interfaces;
+
+namespace SimplyKnowHau.Application.Services
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Hash(string email, string password)
+        {
+            double length = password.Length + email.Length;
+
+            var hash = $"{email[0]}{Math.Pow(length, 3)}{password[4]}";
+
+            return hash;
+        }
+
+        public bool Verify(string passwordHash, string email, string password)
+        {
+            var hash = Hash(email, password);
+            if (hash == passwordHash)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
