1bb0731bb747f6d8775652dc54e674dad770daae
kUcLis <ulis89@gmail.com>
2023-08-29 20:34:11 +0200

    Added AuthenticateModels, Added Authenticate method too userService

---
+++ SimplyKnowHau.Application/AuthenticateModels/AuthenticateResponse.cs
@@ -0,0 +1,22 @@
+﻿using SimplyKnowHau.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplyKnowHau.Application.AuthenticateModels
+{
+    public class AuthenticateResponse
+    {
+        public int UserId { get; set; }
+
+        public string Email { get; set; } = default!;
+
+        public DateTime DateOfBirth { get; set; }
+
+        public Role Role { get; set; } = default!;
+
+        public string Token { get; set; } = default!;
+    }
+}
--- SimplyKnowHau.Application/Services/UserService.cs
+++ SimplyKnowHau.Application/Services/UserService.cs
@@ -1,4 +1,5 @@
-﻿using SimplyKnowHau.Application.Interfaces;
+﻿using SimplyKnowHau.Application.AuthenticateModels;
+using SimplyKnowHau.Application.Interfaces;
 using SimplyKnowHau.Domain.Entities;
 using SimplyKnowHau.Domain.Interfaces;
 
@@ -8,15 +9,32 @@ namespace SimplyKnowHau.Application.Services
     public class UserService : IUserService
     {
         private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher _passwordHasher;
 
-        public UserService(IUserRepository userRepository)
+        public UserService(IUserRepository userRepository, IPasswordHasher passwordHasher)
         {
             _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
         }
 
         public async Task<User> GetById(int id)
         {
             return await _userRepository.GetById(id);
+        }
+
+        public async Task<AuthenticateResponse> Authenticate(AuthenticateRequest request)
+        {
+            var users = await _userRepository.GetAllWithRoles();
+            var user = users.FirstOrDefault(x => x.Email == request.Email && _passwordHasher.Verify(x.PasswordHash, request.Email, request.Password));
+            // return null if user not found
+            if (user == null) return null;
+
+            // authentication successful so generate jwt token
+            var token = generateJwtToken(user);
+
+            var userDto = _mapper.Map<UserDto>(user);
+
+            return new AuthenticateResponse(userDto, token);
         }
     }
 }
