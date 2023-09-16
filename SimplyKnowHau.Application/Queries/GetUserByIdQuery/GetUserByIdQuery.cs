5abd2adc7b265a7c87bf20a9296cc36696c9417a
kUcLis <ulis89@gmail.com>
2023-09-16 21:30:07 +0200

    Added GetUserByIdQuery

---
+++ SimplyKnowHau.Application/Queries/GetUserByIdQuery/GetUserByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using SimplyKnowHau.Application.DTOs;
+
+
+namespace SimplyKnowHau.Application.Queries.GetUserByIdQuery
+{
+    public class GetUserByIdQuery : IRequest<UserDTO>
+    {
+        public int UserId { get; set; }
+    }
+}
---
+++ SimplyKnowHau.Application/Queries/GetUserByIdQuery/GetUserByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using SimplyKnowHau.Application.DTOs;
+using SimplyKnowHau.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplyKnowHau.Application.Queries.GetUserByIdQuery
+{
+    public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, UserDTO>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public GetUserByIdQueryHandler(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<UserDTO> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            return _mapper.Map<UserDTO>(await _userRepository.GetById(request.UserId));
+        }
+    }
+}
