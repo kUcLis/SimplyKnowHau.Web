e008d9db165f93d6f592af76dbc55ec5ef42336d
kUcLis <ulis89@gmail.com>
2023-09-18 18:09:58 +0200

    Added intructions in RoleRepository interface

--- SimplyKnowHau.Infrastructure/Repositories/RoleRepository.cs
+++ SimplyKnowHau.Infrastructure/Repositories/RoleRepository.cs
@@ -1,9 +1,33 @@
-﻿using SimplyKnowHau.Domain.Interfaces;
+﻿using SimplyKnowHau.Domain.Entities;
+using SimplyKnowHau.Domain.Interfaces;
 
 namespace SimplyKnowHau.Infrastructure.Repositories
 {
     public class RoleRepository : IRoleRepository
     {
+        public Task<Role> Add(Role role)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task Delete(Role role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Role?> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Role> Update(Role role)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
